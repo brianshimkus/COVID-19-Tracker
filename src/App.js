d401c2eb0ba38a44bd036c18aa100b342c77cffb
Brian Shimkus <brianshimkus1@gmail.com>
2020-10-19 00:12:15 -0500

    Built out table component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { useEffect, useState } from 'react';
 import { MenuItem, Select, Card } from '@material-ui/core';
 import InfoBox from './Components/InfoBox';
 import Map from './Components/Map';
+import Table from './Components/Table';
 import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
@@ -28,6 +30,7 @@ function App() {
             value: country.countryInfo.iso2, // USA
           }));
 
+          setTableData(data);
           setCountries(countries);
         });
     };
@@ -93,7 +96,7 @@ function App() {
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by Country</h3>
-          {/* {Table} */}
+          <Table countries={tableData} />
           <h3>New cases worldwide</h3>
           {/* {Graph} */}
         </CardContent>
