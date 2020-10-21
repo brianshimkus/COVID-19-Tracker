229738c578e26e02426c4d9722623f91a3e5bf6d
Brian Shimkus <brianshimkus1@gmail.com>
2020-10-21 15:56:07 -0500

    Info Box styling changes on click

--- src/Components/InfoBox.js
+++ src/Components/InfoBox.js
@@ -2,19 +2,23 @@ import React from 'react';
 import '../Styles/InfoBox.css';
 import { Card, CardContent, Typography } from '@material-ui/core';
 
-function InfoBox({ title, cases, total, ...props }) {
+function InfoBox({ title, cases, isRed, active, total, ...props }) {
   return (
-    <Card onClick={props.onClick} className="infoBox">
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && 'infoBox--selected'} ${
+        isRed && 'infoBox--red'
+      }`}
+    >
       <CardContent>
-        {/* {Ttitle} */}
         <Typography className="infoBox__title" color="textSecondary">
           {title}
         </Typography>
 
-        {/* {Number of Cases} */}
-        <h2 className="infoBox__cases">{cases}</h2>
+        <h2 className={`infoBox__cases ${!isRed && 'infoBox__cases--green'}`}>
+          {cases}
+        </h2>
 
-        {/* {Total} */}
         <Typography className="infoBox__total" color="textSecondary">
           {total} Total
         </Typography>
